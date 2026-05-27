e698d1fbb573ca9997dbab8d0993c0e84426a30e
Ben Kuper <bkuperberg@gmail.com>
2026-05-27 10:42:28 +0200

    afterparty

--- AfterParty/scripts/NexusButton.js
+++ AfterParty/scripts/NexusButton.js
@@ -11,7 +11,7 @@ function updateColors(colors, id, resolution, time, params, prop)//, num, speed,
 	var targetProg = (pressed || params.force) ? 1 : 0;
 	if(progression[id] == undefined) progression[id] = 0;
 	progression[id] = progression[id] + (targetProg - progression[id]) / (params.smooth * 100);
-	if (progression[id] >= .99) progression[id] = 1;
+	if (progression[id]  >= .99) progression[id] = 1;
 	if (progression[id] <= .01) progression[id] = 0;
 
 	var color = colors.lerpColor(params.colorOff, params.colorOn, progression[id]);
